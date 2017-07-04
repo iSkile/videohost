c91794bf06ef87eb8062dca26bd63307c721cfdf
Igor Bondarenko <iSkile@ya.ru>
2017-07-04 16:07:51 +0300

    Delete default color

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-                <div style="color:#999;margin:1em 0">
+            <div style="margin:1em 0">
                     If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
                 </div>
 
